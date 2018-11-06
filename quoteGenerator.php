45489d2035dfd3a6845e0fad741b32722f6b466a
Vanessa Asse <vanessa.asse@gmail.com>
2018-11-06 19:11:04 +0100

    [PHP]Ajout du shortcode pour afficher l'index

--- quoteGenerator.php
+++ quoteGenerator.php
@@ -11,6 +11,9 @@ Author: Vanessa Asse
 Version: 1.0
 */
 
+// ajouter le shortcode pour afficher le contenu de l'index.php
+add_shortcode('bienvenue', 'shortcode_index');
+
 // Include le contenu de l'index.php du plugin
 function getPage() {
     ob_start();
@@ -24,8 +27,21 @@ function shortcode_index(){
     return $index;
 }
 
-// ajouter le shortcode pour afficher le contenu de l'index.php
-add_shortcode('bienvenue', 'shortcode_index');
 
+// Ajouter CSS
+add_action( 'wp_enqueue_scripts', 'wpm_enqueue_styles' ); // style theme enfant
+add_action('wp_enqueue_script','Qg_stylesheet'); // style plugin
 
+function wpm_enqueue_styles()
+{
+    wp_enqueue_style( 'Twenty Fifteen', get_template_directory_uri() . '/style.css' ); // style theme enfant
+    wp_enqueue_style( 'quoteGenerator', plugins_url('style.css', __FILE__)  ); // style plugin
+}
+
+add_action('wp_loaded', 'get_page_pack');
+
+function get_page_pack()
+{
+    wp_redirect($url);
+}
 
