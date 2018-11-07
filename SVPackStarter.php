52757e2eb2f75dfa65268d9cc19e3c099b9c8771
Vanessa Asse <vanessa.asse@gmail.com>
2018-11-07 20:03:11 +0100

    [PHP] Test sur le Pack Starter - Gestion de l'envoi du message client à la fin de la demande de devis

--- SVPackStarter.php
+++ SVPackStarter.php
@@ -27,7 +27,10 @@ Cet accompagnement personnalisé comprend :
 <h2>Contactez-nous pour en savoir plus !</h2>
 
 
-<form method="post" action="sendMail/send.php">
+<form method="post" action="">
+
+    <?php wp_nonce_field('sendMsgClient', 'messageClient'); ?>
+
     Nom : <input name="lastname" type="text" required/><br/>
     Prénom : <input name="firstname" type="text" required /><br/>
     Mail :<input name="email" type="email" required /><br/>
@@ -37,7 +40,7 @@ Cet accompagnement personnalisé comprend :
     Message :<textarea name="message" cols="50" rows="5"></textarea><br/><br/>
 
     <input name="pack" type="hidden" value="Site Vitrine : Pack Starter"/>
-    <input type="submit" value="Envoyez" />
+    <input type="submit" name="validateMsgClient" value="Envoyez" />
 </form>
 
 </body>
