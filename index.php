f9fcb9512737fb25d8c664a90d6e4f61c39ed9cd
Vanessa Asse <vanessa.asse@gmail.com>
2018-11-07 13:13:02 +0100

    [JS] Ajout de conditions pour afficher les bonnes questions en fonction  des réponses (site vitrine ou site e-commerce)

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
         <title>Demande de devis - Auverlink</title>
     </head>-->
 
-<body onload="aff_vitrine('non'); aff_commerce('non'); aff_seo('non'), blog_vitrine('non'), aff_marketing('non')"  >
+<body onload="aff_site('oui'); aff_vitrine('non'); aff_commerce('non'); aff_seo('non'), blog_vitrine('non'), aff_marketing('non')"  >
 
 
 <H1>Commencez votre projet web : demandez un devis</H1>
 
-<p>Vous avez un projet web en tête pour votre entreprise mais vous ne savez pas par quoi commencer.<br/>
-    Répondez à formulaire. Vous y trouverez la solution la plus adéquate.</p>
+<p>Vous avez un projet web en tête pour votre entreprise mais vous ne savez pas par quoi commencer.
+    Répondez à ce formulaire. Vous y trouverez la solution adaptée à vos besoins.</p>
 
 <div class="test">
     <p>
@@ -29,14 +29,16 @@
     <?php wp_nonce_field('checkForm', 'checkFormWp'); ?>
 
     <!-- question 1 -->
-    <label>Quel type de site souhaitez-vous ?</label><br/>
-    <input type="radio" name="site" value="Site Vitrine"  onchange="aff_vitrine('oui'); aff_commerce('non')" />Site Vitrine<br/>
-    <input type="radio" name="site" value="Site E-commerce" onchange="aff_vitrine('non'); aff_commerce('oui')"/>Site E-commerce</br>
-             <br/>
+    <div id="block_site">
+        <label>Quel type de site souhaitez-vous ?</label><br/>
+        <input type="radio" name="site" value="Site Vitrine"  onchange="aff_vitrine('oui'); aff_commerce('non'); aff_marketing('non')" />Site Vitrine<br/>
+        <input type="radio" name="site" value="Site E-commerce" onchange="aff_vitrine('non'); aff_commerce('oui'); blog_vitrine('non'); aff_seo('non')"/>Site E-commerce</br>
+                 <br/>
+    </div>
 
     <!-- question 2 si réponse site vitrine -->
     <div id="block_vitrine">
-        <label>Combien de pages contiendra votre site ?</label><br/>
+        <label>Combien de pages contiendra votre site vitrine ?</label><br/>
         <input type="radio" name="pages" value="One page" onchange="aff_seo('oui'); blog_vitrine('non')" />Uniquement 1 page
         <br/>
         <input type="radio" name="pages" value="Five pages sv" onchange="aff_seo('non'); blog_vitrine('oui') "/>Entre 1 et 5 pages
@@ -47,7 +49,7 @@
 
     <!-- question 2 si réponse e-commerce -->
     <div id="block_commerce">
-        <label>Combien de pages contiendra votre site ?</label><br/>
+        <label>Combien de pages contiendra votre site e-commerce ?</label><br/>
         <input type="radio" name="pages" value="Five pages" onchange="aff_marketing('oui')" />Entre 1 et 5 pages
         <br />
         <input type="radio" name="pages" value="Ten pages" onchange="aff_marketing('non')" />De 6 à 10 pages
@@ -66,7 +68,7 @@
     <!-- question 3 si site vitrine et 1 à 10 pages -->
     <!-- question 4 si site vitrine One page et seo oui -->
     <div id="block_blog">
-        <label>Dans votre site, un espace "Blog" sera utile ?</label><br/>
+        <label>Dans votre site, un espace "Blog" sera-t-il utile ?</label><br/>
         <input type="radio" name="blog" value="yes" />Oui
         <br/>
         <input type="radio" name="blog" value="no" />Non
@@ -93,6 +95,11 @@
 
 <!-- Fonctions JS -->
 <script>
+    function aff_site(action)
+    {
+        document.getElementById('block_site').style.display = (action == "oui")? "inline" : "none";
+    }
+
     function aff_vitrine(action){
         document.getElementById('block_vitrine').style.display = (action == "oui")? "inline" : "none";
     }
